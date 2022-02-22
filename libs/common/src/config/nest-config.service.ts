399cc8a4a9742a23b4442be085c563fa2f120379
insungsong <commit@capa.ai>
2022-02-22 11:17:10 +0900

    feat: db

--- libs/common/src/config/nest-config.service.ts
+++ libs/common/src/config/nest-config.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IsEnum } from 'class-validator';
+import { LoggerOptions } from 'typeorm';
 
 export enum Environment {
   DEFAULT = '',
@@ -34,5 +35,40 @@ export class NestConfigService {
 
   get rabbitmqPass(): string | undefined {
     return this.configService.get<string | undefined>('RABBITMQ_PASS');
+  }
+
+  get dbHost(): string {
+    return this.configService.get<string>('DB_HOST', 'localhost');
+  }
+
+  get dbPort(): number {
+    return this.configService.get<number>('DB_PORT', 5432);
+  }
+
+  get dbUsername(): string {
+    return this.configService.get<string>('DB_USERNAME', 'postgres');
+  }
+
+  get dbPassword(): string {
+    return this.configService.get<string>('DB_PASSWORD', 'postgres');
+  }
+
+  get dbDatabase(): string {
+    return this.configService.get<string>('DB_DATABASE', 'postgres');
+  }
+
+  get dbSchema(): string {
+    return this.configService.get<string>('DB_SCHEMA', 'public');
+  }
+
+  get dbSync(): boolean {
+    return this.configService.get<boolean>('DB_SYNC', false);
+  }
+
+  get dbDebug(): LoggerOptions {
+    return this.configService.get<LoggerOptions>(
+      'DB_DEBUG',
+      <LoggerOptions>'error',
+    );
   }
 }
